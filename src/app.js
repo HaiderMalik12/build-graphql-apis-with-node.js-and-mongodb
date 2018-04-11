5b734e3f829a9b5850a7d0da82172181c4982fad
Haider Malik <haidermalik504@gmail.com>
2018-04-11 16:07:09 +0500

    creating-express-server

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
-const start = 'Welcome to node-starter-kit';
-console.log(start);
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    return res.json({
+        msg: 'hello graphql'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
+})
