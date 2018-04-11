8ed871edfc4433ecf97b41410e3dc20aa2937c95
Haider Malik <haidermalik504@gmail.com>
2018-04-11 16:21:20 +0500

    create GraphQL HTTP Server with ExpressJs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
 
 const app = express();
 const PORT = 3000;
 
+const schema = {};
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema
+}));
 app.get('/', (req, res) => {
     return res.json({
         msg: 'hello graphql'
