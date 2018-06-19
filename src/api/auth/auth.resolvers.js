adbf7ce48115dba6025661e663ecb5ca27300f72
Haider Malik <haidermalik504@gmail.com>
2018-06-19 16:05:12 +0500

    authenticate user

--- src/api/auth/auth.resolvers.js
+++ src/api/auth/auth.resolvers.js
@@ -23,8 +23,31 @@ async function signup(_, { input }, ctx, info) {
     }
   };
 }
+async function login(parent, { input }, ctx, info) {
+  const { value, error } = validateUser(input);
+  if (error) {
+    throw new Error(error.message);
+  }
+  const user = await ctx.models.user.findOne({ email: value.email });
+  if (!user) {
+    throw new Error('No such a user');
+  }
+  const matched = await bcrypt.compare(value.password, user.password);
+  if (!matched) {
+    throw new Error('invalid password');
+  }
+  const token = jwt.sign({ userId: user._id }, APP_SECRET);
+  return {
+    token,
+    user: {
+      _id: user._id,
+      email: user.email
+    }
+  };
+}
 export default {
   Mutation: {
-    signup
+    signup,
+    login
   }
 };
