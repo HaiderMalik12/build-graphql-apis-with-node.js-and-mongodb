f6957a3d6cf10c1e5df0fb649b2f6751a8dc0aed
Haider Malik <haidermalik504@gmail.com>
2018-06-20 17:10:52 +0500

    apply authentication middleware on the resolvers

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
 import mongoose from 'mongoose';
-import schema from './schema';
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer } from 'graphql-yoga';
 import graphqlConfig from './api';
+import { makeExecutableSchema } from 'graphql-tools';
+import { applyMiddleware } from 'graphql-middleware';
+import { authMiddleware } from './api/middlewares';
 
 const PORT = 3000;
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/gql_db');
 
+const options = {
+  tracing: true,
+  debug: true,
+  port: PORT,
+  endpoint: '/graphql',
+  playground: '/docs'
+};
 
-const options  = {
-    tracing :true,
-    debug: true,
-    port: PORT,
-    endpoint: '/graphql',
-    playground: '/docs'
-}
-
-const server = new GraphQLServer(graphqlConfig)
-server.start(options,() => console.log('Server is running on localhost:3000'))
+//create a schema
+const schema = makeExecutableSchema({
+  typeDefs: graphqlConfig.typeDefs,
+  resolvers: graphqlConfig.resolvers
+});
+//apply middlewares on the schema
+const protectedSchema = applyMiddleware(schema, authMiddleware);
+//provided the protected Schema to graphql Server
+const server = new GraphQLServer({
+  schema: protectedSchema,
+  context: graphqlConfig.context
+});
+server.start(options, () => console.log('Server is running on localhost:3000'));
