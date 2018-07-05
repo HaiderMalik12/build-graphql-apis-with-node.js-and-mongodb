7419eb0a9d87a0daed49f19bf77f917228308e94
Haider Malik <haidermalik504@gmail.com>
2018-07-05 12:53:24 +0500

    connect with mongoDb using mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+
 import schema from './schema';
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/gql_db', { useNewUrlParser: true });
 
 const app = express();
 const PORT = 3000;
---
+++ src/models/product.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    qty: {
+        type: Number
+    }
+});
+export default mongoose.model('product', ProductSchema);
