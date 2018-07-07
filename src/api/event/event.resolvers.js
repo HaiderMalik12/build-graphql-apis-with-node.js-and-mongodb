93bb9461275b0aa6ae3b099d62b3d0547a8c7ff5
Haider Malik <haidermalik504@gmail.com>
2018-07-07 09:00:10 +0500

    create interfaces

--- src/api/event/event.resolvers.js
+++ src/api/event/event.resolvers.js
@@ -1,9 +1,12 @@
 export default {
   Query: {
+    async findEventsAtVenue(_, { venue }, { models }) {
+      return await models.event.find({ venue });
+    }
   },
   Mutation: {
     async newEvent(_, { input }, { models }) {
       return await models.event.create(input);
     }
-  }
+  },
 };
