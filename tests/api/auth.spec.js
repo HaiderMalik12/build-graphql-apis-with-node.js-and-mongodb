fe97499003770692e229c66d98f27ad5574246b2
Haider Malik <haidermalik504@gmail.com>
2018-07-07 20:20:55 +0500

    write test for signup mutation

--- tests/api/auth.spec.js
+++ tests/api/auth.spec.js
@@ -2,8 +2,38 @@
 import axios from 'axios';
 import { clearCollection, disConneectMongo } from '../db';
 
-describe('resolvers', () => {
+describe.only('resolvers', () => {
+    beforeEach(clearCollection);
+    afterAll(disConneectMongo);
     test('should create a new account for the user', async () => {
-        expect(1).toBe(1);
+        const response = await axios.post('http://localhost:3000/graphql', {
+            query:
+                `mutation Signup($input: AuthInput!)
+            {
+              signup(input: $input){
+                user{
+                  email
+                }
+              }
+            }`,
+            variables: {
+                "input": {
+                    "email": "jane_doe@gmail.com",
+                    "password": "password"
+                }
+            }
+        },
+        );
+        expect(response.status).toBe(200);
+        expect(response.data).toMatchObject({
+            "data": {
+                "signup": {
+                    "user": {
+                        "email": "jane_doe@gmail.com"
+                    }
+                }
+            }
+        })
+
     })
 });
